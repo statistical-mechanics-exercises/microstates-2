b1dcf594c4ac6a3c8ebdbd624a0049c2bd26ee01
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-15 12:12:05 +0000

    Added answer

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import numpy as np
 def compute_b( spins ) :
   b = 0
   # Your code goes here
-  
+  for i in range(len(spins)) : 
+      if spins[i]==1 : b = b + 2**i 
   return b
   
 print( compute_b([1,1,1]) )
